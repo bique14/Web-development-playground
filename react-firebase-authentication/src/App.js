85f7ac9af502d11298f5849b727222071e3d4138
bique14 <peerasorn14@gmail.com>
2018-10-09 01:05:19 +0700

    done

--- react-firebase-authentication/src/App.js
+++ react-firebase-authentication/src/App.js
@@ -1,21 +1,85 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import firebase from 'firebase'
+
+import './App.css'
+import { CONFIG } from './firebase'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      age: 0,
+      list: []
+    }
+    
+    firebase.initializeApp(CONFIG)
+    let db = firebase.database().ref('/name')
+    db.on('value', snapshot => {
+      this.setState({
+        list: snapshot.val()
+      })
+    })
+  }
+
+  onNameChange(e) {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  onAgeChange(e) {
+    this.setState({
+      age: e === 'increase' ? this.state.age + 1 : this.state.age - 1
+    })
+  }
+
+  onSubmit() {
+    let db = firebase.database().ref('/name')
+    const { name, age } = this.state
+    db.push({
+      name,
+      age
+    })
+
+  }
+
   render() {
+    const { name, age, list } = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <h1>Name</h1>
+          <input type="text" onChange={this.onNameChange.bind(this)}></input>
+          <h3>{name}</h3>
+        </div>
+        <div>
+          <h1>Age</h1>
+          <button onClick={() => this.onAgeChange("increase")}>+</button>
+          <p>{age}</p>
+          <button onClick={() => this.onAgeChange("decrease")}>-</button>
+        </div>
+        <button onClick={this.onSubmit.bind(this)}>submit</button>
+
+        <table className="table">
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+          {
+            Object.keys(list).map((data, index) => {
+              return (
+                <tr>
+                  <td>{list[data].name}</td>
+                  <td>{list[data].age}</td>
+                </tr>
+              )
+            })
+          }
+        </table>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ react-firebase-authentication/src/firebase.js
@@ -0,0 +1,16 @@
+// import firebase from 'firebase'
+
+export const CONFIG = {
+  apiKey: "AIzaSyBkZDT0FpqTL_PHc2kCtcYwAea8DPTZyX8",
+  authDomain: "myworld-436f2.firebaseapp.com",
+  databaseURL: "https://myworld-436f2.firebaseio.com",
+  projectId: "myworld-436f2",
+  storageBucket: "myworld-436f2.appspot.com",
+  messagingSenderId: "579359171391"
+}
+
+// module.exports = {
+//   CONFIG
+// }
+
+// firebase.initializeApp(CONFIG)
