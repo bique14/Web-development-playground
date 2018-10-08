1214da2226089bf8589c74b72dc36e2f4f3081e3
bique14 <peerasorn14@gmail.com>
2018-10-09 01:06:38 +0700

    remove key

--- react-firebase-authentication/src/firebase.js
+++ react-firebase-authentication/src/firebase.js
@@ -1,16 +1,8 @@
-// import firebase from 'firebase'
-
 export const CONFIG = {
-  apiKey: "AIzaSyBkZDT0FpqTL_PHc2kCtcYwAea8DPTZyX8",
-  authDomain: "myworld-436f2.firebaseapp.com",
-  databaseURL: "https://myworld-436f2.firebaseio.com",
-  projectId: "myworld-436f2",
-  storageBucket: "myworld-436f2.appspot.com",
-  messagingSenderId: "579359171391"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 }
-
-// module.exports = {
-//   CONFIG
-// }
-
-// firebase.initializeApp(CONFIG)
